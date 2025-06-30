a362d4b95fc4e817f0319cffd9fcc6167baca34c
Dany Khalife <dev@dkhalife.com>
2025-06-29 19:42:33 -0700

    Address review comments

--- internal/apis/task.go
+++ internal/apis/task.go
@@ -68,6 +68,40 @@ func (h *TasksAPIHandler) getTasks(c *gin.Context) {
 	})
 }
 
+func (h *TasksAPIHandler) getCompletedTasks(c *gin.Context) {
+	currentIdentity := auth.CurrentIdentity(c)
+
+	log := logging.FromContext(c)
+
+	limitStr := c.DefaultQuery("limit", "10")
+	pageStr := c.DefaultQuery("page", "1")
+
+	limit, err := strconv.Atoi(limitStr)
+	if err != nil || limit <= 0 {
+		c.Status(http.StatusBadRequest)
+		return
+	}
+
+	page, err := strconv.Atoi(pageStr)
+	if err != nil || page <= 0 {
+		c.Status(http.StatusBadRequest)
+		return
+	}
+
+	offset := (page - 1) * limit
+
+	tasks, err := h.tRepo.GetCompletedTasks(c, currentIdentity.UserID, limit, offset)
+	if err != nil {
+		log.Errorf("error getting completed tasks: %s", err.Error())
+		c.Status(http.StatusInternalServerError)
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"tasks": tasks,
+	})
+}
+
 func (h *TasksAPIHandler) getTask(c *gin.Context) {
 	currentIdentity := auth.CurrentIdentity(c)
 
@@ -538,6 +572,7 @@ func TaskRoutes(router *gin.Engine, h *TasksAPIHandler, auth *jwt.GinJWTMiddlewa
 	tasksRoutes.Use(auth.MiddlewareFunc())
 	{
 		tasksRoutes.GET("/", authMW.ScopeMiddleware(models.ApiTokenScopeTaskRead), h.getTasks)
+		tasksRoutes.GET("/completed", authMW.ScopeMiddleware(models.ApiTokenScopeTaskRead), h.getCompletedTasks)
 		tasksRoutes.PUT("/", authMW.ScopeMiddleware(models.ApiTokenScopeTaskWrite), h.editTask)
 		tasksRoutes.POST("/", authMW.ScopeMiddleware(models.ApiTokenScopeTaskWrite), h.createTask)
 		tasksRoutes.GET("/:id", authMW.ScopeMiddleware(models.ApiTokenScopeTaskRead), h.getTask)
