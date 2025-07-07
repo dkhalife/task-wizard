4303e1e1721b2fe1b3787cbd6cb965b7b14daef9
Dany Khalife <dev@dkhalife.com>
2025-07-06 20:10:29 -0700

    Name DB indexes for clarity

--- internal/models/label.go
+++ internal/models/label.go
@@ -6,7 +6,7 @@ type Label struct {
 	ID        int        `json:"id" gorm:"primary_key"`
 	Name      string     `json:"name" gorm:"column:name;not null"`
 	Color     string     `json:"color" gorm:"type:varchar(7);column:color;not null"`
-	CreatedBy int        `json:"created_by" gorm:"column:created_by;not null;index"`
+	CreatedBy int        `json:"created_by" gorm:"column:created_by;not null;index:idx_labels_created_by"`
 	CreatedAt time.Time  `json:"-" gorm:"column:created_at;default:CURRENT_TIMESTAMP"`
 	UpdatedAt *time.Time `json:"-" gorm:"column:updated_at;default:NULL;autoUpdateTime"`
 
