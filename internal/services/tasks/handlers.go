b4ce5d55d5e59ffa3660dd0c9c73c12626bc71f4
Dany Khalife <dev@dkhalife.com>
2025-07-12 22:08:55 +0000

    make multiline

--- internal/services/tasks/handlers.go
+++ internal/services/tasks/handlers.go
@@ -22,7 +22,10 @@ func NewTasksMessageHandler(ts *TaskService) *TasksMessageHandler {
 
 func (h *TasksMessageHandler) getUserTasks(ctx context.Context, userID int, _ ws.WSMessage) *ws.WSResponse {
 	status, response := h.ts.GetUserTasks(ctx, userID)
-	return &ws.WSResponse{Status: status, Data: response}
+	return &ws.WSResponse{
+		Status: status,
+		Data:   response,
+	}
 }
 
 func (h *TasksMessageHandler) getCompletedTasks(ctx context.Context, userID int, msg ws.WSMessage) *ws.WSResponse {
@@ -31,7 +34,12 @@ func (h *TasksMessageHandler) getCompletedTasks(ctx context.Context, userID int,
 		Page  int `json:"page"`
 	}
 	if err := json.Unmarshal(msg.Data, &req); err != nil {
-		return &ws.WSResponse{Status: http.StatusBadRequest, Data: &gin.H{"error": "Invalid request data"}}
+		return &ws.WSResponse{
+			Status: http.StatusBadRequest,
+			Data: gin.H{
+				"error": "Invalid request data",
+			},
+		}
 	}
 	if req.Limit <= 0 {
 		req.Limit = 10
@@ -46,46 +54,86 @@ func (h *TasksMessageHandler) getCompletedTasks(ctx context.Context, userID int,
 func (h *TasksMessageHandler) getTask(ctx context.Context, userID int, msg ws.WSMessage) *ws.WSResponse {
 	var id int
 	if err := json.Unmarshal(msg.Data, &id); err != nil {
-		return &ws.WSResponse{Status: http.StatusBadRequest, Data: &gin.H{"error": "Invalid task ID"}}
+		return &ws.WSResponse{
+			Status: http.StatusBadRequest,
+			Data: gin.H{
+				"error": "Invalid task ID",
+			},
+		}
 	}
 	status, response := h.ts.GetTask(ctx, userID, id)
-	return &ws.WSResponse{Status: status, Data: response}
+	return &ws.WSResponse{
+		Status: status,
+		Data:   response,
+	}
 }
 
 func (h *TasksMessageHandler) createTask(ctx context.Context, userID int, msg ws.WSMessage) *ws.WSResponse {
 	var req models.CreateTaskReq
 	if err := json.Unmarshal(msg.Data, &req); err != nil {
-		return &ws.WSResponse{Status: http.StatusBadRequest, Data: &gin.H{"error": "Invalid request data"}}
+		return &ws.WSResponse{
+			Status: http.StatusBadRequest,
+			Data: gin.H{
+				"error": "Invalid request data",
+			},
+		}
 	}
 	status, response := h.ts.CreateTask(ctx, userID, req)
-	return &ws.WSResponse{Status: status, Data: response}
+	return &ws.WSResponse{
+		Status: status,
+		Data:   response,
+	}
 }
 
 func (h *TasksMessageHandler) updateTask(ctx context.Context, userID int, msg ws.WSMessage) *ws.WSResponse {
 	var req models.UpdateTaskReq
 	if err := json.Unmarshal(msg.Data, &req); err != nil {
-		return &ws.WSResponse{Status: http.StatusBadRequest, Data: &gin.H{"error": "Invalid request data"}}
+		return &ws.WSResponse{
+			Status: http.StatusBadRequest,
+			Data: gin.H{
+				"error": "Invalid request data",
+			},
+		}
 	}
 	status, response := h.ts.EditTask(ctx, userID, req)
-	return &ws.WSResponse{Status: status, Data: response}
+	return &ws.WSResponse{
+		Status: status,
+		Data:   response,
+	}
 }
 
 func (h *TasksMessageHandler) deleteTask(ctx context.Context, userID int, msg ws.WSMessage) *ws.WSResponse {
 	var id int
 	if err := json.Unmarshal(msg.Data, &id); err != nil {
-		return &ws.WSResponse{Status: http.StatusBadRequest, Data: &gin.H{"error": "Invalid task ID"}}
+		return &ws.WSResponse{
+			Status: http.StatusBadRequest,
+			Data: gin.H{
+				"error": "Invalid task ID",
+			},
+		}
 	}
 	status, response := h.ts.DeleteTask(ctx, userID, id)
-	return &ws.WSResponse{Status: status, Data: response}
+	return &ws.WSResponse{
+		Status: status,
+		Data:   response,
+	}
 }
 
 func (h *TasksMessageHandler) skipTask(ctx context.Context, userID int, msg ws.WSMessage) *ws.WSResponse {
 	var id int
 	if err := json.Unmarshal(msg.Data, &id); err != nil {
-		return &ws.WSResponse{Status: http.StatusBadRequest, Data: &gin.H{"error": "Invalid task ID"}}
+		return &ws.WSResponse{
+			Status: http.StatusBadRequest,
+			Data: gin.H{
+				"error": "Invalid task ID",
+			},
+		}
 	}
 	status, response := h.ts.SkipTask(ctx, userID, id)
-	return &ws.WSResponse{Status: status, Data: response}
+	return &ws.WSResponse{
+		Status: status,
+		Data:   response,
+	}
 }
 
 func (h *TasksMessageHandler) updateDueDate(ctx context.Context, userID int, msg ws.WSMessage) *ws.WSResponse {
@@ -94,37 +142,69 @@ func (h *TasksMessageHandler) updateDueDate(ctx context.Context, userID int, msg
 		models.UpdateDueDateReq
 	}
 	if err := json.Unmarshal(msg.Data, &req); err != nil {
-		return &ws.WSResponse{Status: http.StatusBadRequest, Data: &gin.H{"error": "Invalid request data"}}
+		return &ws.WSResponse{
+			Status: http.StatusBadRequest,
+			Data: gin.H{
+				"error": "Invalid request data",
+			},
+		}
 	}
 	status, response := h.ts.UpdateDueDate(ctx, userID, req.ID, req.UpdateDueDateReq)
-	return &ws.WSResponse{Status: status, Data: response}
+	return &ws.WSResponse{
+		Status: status,
+		Data:   response,
+	}
 }
 
 func (h *TasksMessageHandler) completeTask(ctx context.Context, userID int, msg ws.WSMessage) *ws.WSResponse {
 	var id int
 	if err := json.Unmarshal(msg.Data, &id); err != nil {
-		return &ws.WSResponse{Status: http.StatusBadRequest, Data: &gin.H{"error": "Invalid task ID"}}
+		return &ws.WSResponse{
+			Status: http.StatusBadRequest,
+			Data: gin.H{
+				"error": "Invalid task ID",
+			},
+		}
 	}
 	status, response := h.ts.CompleteTask(ctx, userID, id)
-	return &ws.WSResponse{Status: status, Data: response}
+	return &ws.WSResponse{
+		Status: status,
+		Data:   response,
+	}
 }
 
 func (h *TasksMessageHandler) uncompleteTask(ctx context.Context, userID int, msg ws.WSMessage) *ws.WSResponse {
 	var id int
 	if err := json.Unmarshal(msg.Data, &id); err != nil {
-		return &ws.WSResponse{Status: http.StatusBadRequest, Data: &gin.H{"error": "Invalid task ID"}}
+		return &ws.WSResponse{
+			Status: http.StatusBadRequest,
+			Data: gin.H{
+				"error": "Invalid task ID",
+			},
+		}
 	}
 	status, response := h.ts.UncompleteTask(ctx, userID, id)
-	return &ws.WSResponse{Status: status, Data: response}
+	return &ws.WSResponse{
+		Status: status,
+		Data:   response,
+	}
 }
 
 func (h *TasksMessageHandler) getTaskHistory(ctx context.Context, userID int, msg ws.WSMessage) *ws.WSResponse {
 	var id int
 	if err := json.Unmarshal(msg.Data, &id); err != nil {
-		return &ws.WSResponse{Status: http.StatusBadRequest, Data: &gin.H{"error": "Invalid task ID"}}
+		return &ws.WSResponse{
+			Status: http.StatusBadRequest,
+			Data: gin.H{
+				"error": "Invalid task ID",
+			},
+		}
 	}
 	status, response := h.ts.GetTaskHistory(ctx, userID, id)
-	return &ws.WSResponse{Status: status, Data: response}
+	return &ws.WSResponse{
+		Status: status,
+		Data:   response,
+	}
 }
 
 // TaskMessages registers websocket handlers for task actions.
