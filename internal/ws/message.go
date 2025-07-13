98e34fa79a64f7638458220ce45a3bfaee38e8aa
Dany Khalife <dev@dkhalife.com>
2025-07-12 17:27:18 -0700

    Add request ID to websocket types (#172)

--- internal/ws/message.go
+++ internal/ws/message.go
@@ -4,13 +4,15 @@ import "encoding/json"
 
 // WSMessage represents a request coming from the websocket client.
 type WSMessage struct {
-	Action string          `json:"action"`
-	Data   json.RawMessage `json:"data,omitempty"`
+	RequestID string          `json:"requestId"`
+	Action    string          `json:"action"`
+	Data      json.RawMessage `json:"data,omitempty"`
 }
 
 // WSResponse represents a message sent back to the client.
 type WSResponse struct {
-	Action string      `json:"action"`
-	Status int         `json:"status"`
-	Data   interface{} `json:"data,omitempty"`
+	RequestID string      `json:"requestId,omitempty"`
+	Action    string      `json:"action"`
+	Status    int         `json:"status"`
+	Data      interface{} `json:"data,omitempty"`
 }
