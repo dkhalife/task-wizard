94e777e7c6a593bbc538bd0a073bbdd92e6a24c2
Dany Khalife <dev@dkhalife.com>
2026-06-16 06:44:59 +0300

    fix(apiserver): set session cookie Secure flag from request scheme
    Derive the Secure attribute from the effective request scheme (honoring trusted forwarded headers and direct TLS) instead of the configured host name, so cookies stay Secure behind TLS-terminating proxies while plain HTTP dev still works.

--- apiserver/internal/apis/user.go
+++ apiserver/internal/apis/user.go
@@ -132,7 +132,7 @@ func (h *UsersAPIHandler) CreateSession(c *gin.Context) {
 		return
 	}
 
-	secure := h.cfg.Server.HostName != ""
+	secure := middleware.EffectiveScheme(c) == "https"
 	c.SetSameSite(http.SameSiteLaxMode)
 	c.SetCookie(authMW.SessionCookieName, rawToken, int(duration.Seconds()), "/", "", secure, true)
 	c.Status(http.StatusCreated)
@@ -144,7 +144,7 @@ func (h *UsersAPIHandler) DeleteCurrentSession(c *gin.Context) {
 		_ = h.sessionRepo.DeleteSession(c, sessionToken)
 	}
 
-	secure := h.cfg.Server.HostName != ""
+	secure := middleware.EffectiveScheme(c) == "https"
 	c.SetSameSite(http.SameSiteLaxMode)
 	c.SetCookie(authMW.SessionCookieName, "", -1, "/", "", secure, true)
 	c.Status(http.StatusNoContent)
